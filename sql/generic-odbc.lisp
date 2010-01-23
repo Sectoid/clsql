79e485715e687d9080681ac424bf660f7df63b07
Nathan Bird <nathan@acceleration.net>
2010-01-23 15:52:48 -0500

    Updating logic for odbc database-list-{tables,views}   TODO: revisit owner/schema logic. SqlServer 2005 added more system schemas that need to be ignored. Additionally I'm seeing that with the move from 2000->2005 sqlserver has separated owner from schema and so this will probably need to be revisited further.

--- sql/generic-odbc.lisp
+++ sql/generic-odbc.lisp
@@ -212,34 +212,44 @@
 
 (defmethod database-list-tables ((database generic-odbc-database)
                                  &key (owner nil))
-  (declare (ignore owner))
   (multiple-value-bind (rows col-names)
       (funcall (list-all-database-tables-fn database) :db (odbc-conn database))
     (declare (ignore col-names))
     ;; TABLE_SCHEM is hard-coded in second column by ODBC Driver Manager
     ;; TABLE_NAME in third column, TABLE_TYPE in fourth column
-    (loop for row in rows
-          when (and (not (string-equal "information_schema" (nth 1 row)))
-                    (string-equal "TABLE" (nth 3 row))
+    (loop for (category schema name type . rest) in rows
+          when (and (string-equal "TABLE" type)
+		    (or (null owner) (string-equal owner schema))
+		    ;; unless requesting by name, skip system schemata
+		    (not (and (null owner) 
+			      (member schema '("information_schema" "sys")
+					       :test #'string-equal)))
+		    ;; skip system specific tables in mssql2000
                     (not (and (eq :mssql (database-underlying-type database))
-                              (string-equal "dtproperties" (nth 2 row)))))
-          collect (nth 2 row))))
+                              (string-equal "dtproperties" name))))
+	    collect name)))
 
 
 (defmethod database-list-views ((database generic-odbc-database)
-                                 &key (owner nil))
+				&key (owner nil))
   (multiple-value-bind (rows col-names)
       (funcall (list-all-database-tables-fn database) :db (odbc-conn database))
     (declare (ignore col-names))
     ;; TABLE_SCHEM is hard-coded in second column by ODBC Driver Manager
     ;; TABLE_NAME in third column, TABLE_TYPE in fourth column
-    (loop for (category view-owner name type . rest) in rows
-          when (and (or (null owner) (string-equal owner view-owner) )
-		    (not (string-equal "information_schema" view-owner))
-                    (string-equal "VIEW" type)
-                    (not (and (eq :mssql (database-underlying-type database))
-                              (member name '("sysconstraints" "syssegments") :test #'string-equal))))
-          collect name)))
+    (loop for (category schema name type . rest) in rows
+          when (and (string-equal "VIEW" type)
+		    (or (null owner) (string-equal owner schema))
+		    ;; unless requesting by name, skip system schemata
+		    (not (and (null owner) 
+			      (member schema '("information_schema" "sys")
+				      :test #'string-equal)))
+		    ;; skip system specific tables in mssql2000
+		    (not (and (eq :mssql (database-underlying-type database))
+			      (member name '("sysconstraints" "syssegments"
+					     :test #'string-equal)))))
+	    collect name)))
+hema logic.:sql/generic-odbc.lisp
 
 
 (defmethod database-list-attributes ((table string) (database generic-odbc-database)
